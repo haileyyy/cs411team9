545e45fe7c1cada85a245deefa0b786f6231994a
haileyyy <49726325+haileyyy@users.noreply.github.com>
2021-04-16 12:12:24 -0400

    Update schema.sql

--- app/src/schema.sql
+++ app/src/schema.sql
@@ -1,5 +1,5 @@
 create table user (
-user_ID int(10),
+user_ID int(10) auto increment,
 user_name varchar(50),
 birthdate date, 
 password varchar(10),
@@ -9,12 +9,13 @@ primary key (user_ID)
 create table userService(
 user_ID int (10),
 service_ID int (10),
+primary key (user_ID, service_ID),
 foreign key (service_ID) references service(service_ID),
 foreign key (user_ID) references user(user_ID)
 ) ;
 
 create table service(
-service_ID int (10),
+service_ID int (10) auto increment,
 service_name varchar(50),
 primary key (service_ID)
 )
