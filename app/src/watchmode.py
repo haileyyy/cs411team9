beca3e0916aeffd7ddadb8b21297bc5a0083b8d5
Sam DeCosta <samdeco14@gmail.com>
2021-04-23 12:02:23 -0400

    removing unnecessary watchmode api functions

--- app/src/watchmode.py
+++ app/src/watchmode.py
@@ -17,7 +17,7 @@ def get_genres():
             genres += [[str(genre.get("id")), genre.get('name')]]
         return genres
     else:
-        raise Exception('Watchmode API gave status code {}'.format(response.status_code))
+        raise Exception('TMDB API gave status code {}'.format(response.status_code))
 
 def movies_from_genres(genres,num):
     """Returns num movies from each listed genre"""
@@ -73,21 +73,6 @@ def tmdbID_from_imdbID(imdbID):
     else:
         raise Exception('TMDB API gave status code {}'.format(response.status_code))
 
-def watchmodeID_from_imdbID(imdbID):
-    """
-    Takes in imdbID as string, returns watchmode ID as string
-    """
-    response = requests.get("https://api.watchmode.com/v1/search/?apiKey=" + watchmode_api_key + "&search_field=imdb_id&search_value=" + imdbID)
-
-    if response.status_code == 200:
-        data = response.json()
-        if len(data.get('title_results')) > 0:
-            return str((data.get('title_results')[0].get('id')))
-        else:
-            return "Not found in Watchmode database"
-    else:
-        raise Exception('Watchmode API gave status code {}'.format(response.status_code))
-
 def sources_from_tmdbID(tmdbID):
     """
     Takes in tmdbID as string, returns dictionary of sources 
@@ -98,38 +83,12 @@ def sources_from_tmdbID(tmdbID):
         data = response.json()['results']['US']['flatrate']
         services = []
         for source in data:
-            # can change web_url to any of the parameters in the doc string
             services.append(source['provider_name'])
         return services
     else:
         raise Exception('TMDB API gave status code  {}'.format(response.status_code))
 
-def sources_from_watchmodeID(watchmodeID):
-    """
-    Takes in imdbID as string, returns dictionary of sources (source_id, type, region, ios_url, android_url, web_url, format, price, seasons, episodes)
-    """
-    response = requests.get("https://api.watchmode.com/v1/title/" + watchmodeID + "/sources/?apiKey=" + watchmode_api_key)
 
-    if response.status_code == 200:
-        data = response.json()
-        services = []
-        for source in data:
-            # can change web_url to any of the parameters in the doc string
-            services.append(source['web_url'])
-        return services
-    else:
-        raise Exception('Watchmode API gave status code  {}'.format(response.status_code))
-
-def known_sources(watchmodeSources):
-    sources = []
-    for source in watchmodeSources:
-        if 'netflix' in source and 'Netflix' not in sources:
-            sources.append('Netflix')
-        elif 'amazon' in source and 'Amazon' not in sources:
-            sources.append('Amazon')
-        elif 'hulu' in source and 'Hulu' not in sources:
-            sources.append('Hulu')
-    return sources
         
 
 
