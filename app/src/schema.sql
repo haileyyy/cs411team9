83f58d074d25322c54ba9767d39d7c84e3a383e0
Kevin Tu <57199141+kevintu2@users.noreply.github.com>
2021-04-30 11:56:10 -0400

    checklist

--- app/src/schema.sql
+++ app/src/schema.sql
@@ -1,4 +1,4 @@
-create database userInfo;
+create database userInfo; 
 use userInfo;
 
 create table user (
@@ -13,7 +13,7 @@ create table service(
 service_ID int (10) auto_increment,
 service_name varchar(50),
 primary key (service_ID)
-)
+);
 
 create table userService(
 user_ID int (10),
@@ -22,3 +22,28 @@ primary key (user_ID, service_ID),
 foreign key (service_ID) references service(service_ID),
 foreign key (user_ID) references user(user_ID)
 ) ;
+
+create table genre(
+genre_ID int(10) auto_increment,
+genre_name varchar(50),
+primary key (genre_ID)
+);
+
+create table
+
+create table userScore(
+user_ID int(10),
+genre_ID int(10),
+user_score int(10),
+primary key (user_ID, genre_ID),
+foreign key (genre_ID) references genre(genre_ID),
+foreign key (user_ID) references user(user_ID)
+);
+
+create table watchedMovies(
+user_ID int(10),
+movie_ID varchar(50),
+liked boolean,
+primary key (user_ID, movie_ID),
+foreign key (user_ID) references user(user_ID)
+);
