5f4396c9f867ca637fc3e72b3c9d84363fba750d
zachbodi <zachbodi@bu.edu>
2021-05-02 17:59:10 -0400

    added MySQL connection settings and user cookie generation

--- app/src/app.py
+++ app/src/app.py
@@ -1,24 +1,17 @@
 from secrets import SECRETS
 
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, make_response, request
 from flask_dance.contrib.google import make_google_blueprint, google
-from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
 from flaskext.mysql import MySQL
 import os
 
 app = Flask(__name__, template_folder="templates")
-mysql = MySQL()
 
 # Ignore that connection is not HTTPS (for local testing)
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '130Barnes'
-app.config['MYSQL_DATABASE_DB'] = 'userInfo'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
-
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 
+# Setup Google OAuth
 app.secret_key = SECRETS['flask_secret_key']
 app.config['GOOGLE_OAUTH_CLIENT_ID'] = SECRETS['google_client_id']
 app.config['GOOGLE_OAUTH_CLIENT_SECRET'] = SECRETS['google_client_secret']
@@ -26,29 +19,50 @@ app.config['GOOGLE_OAUTH_CLIENT_SECRET'] = SECRETS['google_client_secret']
 google_bp = make_google_blueprint(scope=['email'], offline=True)
 app.register_blueprint(google_bp, url_prefix='/login')
 
+# Setup MySQL database connection settings
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = SECRETS['db-username']
+app.config['MYSQL_DATABASE_PASSWORD'] = SECRETS['db-password']
+app.config['MYSQL_DATABASE_DB'] = 'movierecommendation'
+app.config['MYSQL_DATABASE_HOST'] = SECRETS['db-endpoint']
+mysql.init_app(app)
+
+# Initalize connection to MySQL database
 conn = mysql.connect()
 cursor = conn.cursor()
 
 def isEmailUnique(email):
     cursor = conn.cursor()
-    if cursor.execute("SELECT user_email  FROM user WHERE user_email = '{0}'".format(email)):
+    if cursor.execute('SELECT email FROM user WHERE email = "{0}"'.format(email)):
         return False
     else:
         return True
 
 @app.route('/')
 def index():
-    if google.authorized:
-        resp = google.get('/oauth2/v1/userinfo')
-        assert resp.ok, resp.text
-        if isEmailUnique(resp.json()['email']) == True:
-            cursor.execute("INSERT INTO user (user_email) VALUES ('{0}')" .format(resp.json()['email']))
+    if request.cookies.get('user_ID', None):
+        return render_template('home.html')
+    elif google.authorized:
+        google_resp = google.get('/oauth2/v1/userinfo')
+        assert google_resp.ok, google_resp.text
+        del google_bp.token
+
+        email = google_resp.json()['email']
+
+        if isEmailUnique(email) == True:
+            cursor.execute('INSERT INTO user (email) VALUES ("{}")'.format(email))
             conn.commit()
-        return render_template('home.html',email=resp.json()['email'])
+
+        cursor.execute('SELECT user_ID FROM user WHERE email="{}"'.format(email))
+        for x in cursor:
+            user_ID = x[0]
+
+        resp = make_response(render_template('home.html'))
+        resp.set_cookie('user_ID', str(user_ID))
+
+        return resp
     else:
         return render_template('index.html')
-
-
 
 @app.route('/login')
 def login():
@@ -56,12 +70,9 @@ def login():
 
 @app.route('/logout')
 def logout():
-    try:
-        del google_bp.token
-    except:
-        print('no Google OAuth token to delete')
-
-    return redirect('/')
+    resp = make_response(render_template('logout.html'))
+    resp.set_cookie('user_ID', '', expires=0)
+    return resp
 
 if __name__ == "__main__":
     app.debug = True
