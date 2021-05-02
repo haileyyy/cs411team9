a870425d75fd8a7c05d65b582f14c728192ea7de
Sam DeCosta <samdeco14@gmail.com>
2021-05-01 21:53:48 -0400

    added genre algorithm for user dashboard

--- app/src/watchmode.py
+++ app/src/watchmode.py
@@ -3,7 +3,7 @@ import json
 from  secrets import SECRETS
 
 watchmode_api_key = SECRETS['watchmode_api_key']
-TMDB_api_key = SECRETS['tmdb_api_key']
+TMDB_api_key = SECRETS['TMDB_api_key']
 
 
 def get_genres():
@@ -37,15 +37,16 @@ def movies_from_genres(genres,num):
     return movies
 
 
-def initial_movie_display(movies_from_genres):
-    movies = []
-    titles = []
-    for genre in movies_from_genres:
-        for movie in movies_from_genres[genre]:
-            if movie[1] not in titles:
-                movies += [[genre] + movie]
-                titles += [movie[1]]
-    return movies
+def initial_movie_display():
+    genres = get_genres()
+    genrescore = {}
+    for genre in genres:
+       genrescore[genre[0]] = 0.05 
+    services = []
+    num_movies = 30
+    watched_movies = []
+    return default_movies_for_user(genrescore,services, num_movies, watched_movies)
+
 
 def get_names_from_movies(movies):
     movielist = []
@@ -54,33 +55,51 @@ def get_names_from_movies(movies):
     return movielist
 
 def test():
-    # get list of all genres
-    genres = get_genres()
-    # get 3 movies from each genre
-    movie_genre_list = movies_from_genres(genres,3)
-    # create one list of movies from those genres for display, with format [genre, ID, title]
-    return initial_movie_display(movie_genre_list)
+    gscore ={}
+    gscore['28'] = 0.2
+    services = ['384']
+    watched_movies = ['Iron man']
+    gscore['12'] = 0.5
+    print(default_movies_for_user(gscore,services,10,watched_movies))
             
 
 
-def default_movies_for_user(genrescore,services, multFactor):
+def default_movies_for_user(genrescore,services, num_movies, watched_movies):
     """takes in list of genrescore from user, users services, and a multFactor and builds up a list of possible movies that are on
         the users services. The number of movies from each genre is the multFactor * their genrescore for that genre"""
     
-    movies = []
+    movies = {}
+
     
     for genre in genrescore:
-   
-        if genre[0] > 0:
-            movies_in_genre = []
-            i = 1
-            while len(movies_in_genre) < multFactor * genre[0]:
-                movie = movies_from_genres([genre[1]],i)
-                actual_movie = [movie[genre[1][1]][-1]]
-                if actual_movie[0] not in movies:
-                    movies_in_genre += actual_movie
-                i += 1
-        movies += movies_in_genre
+        genrescore[genre] = int((genrescore[genre] * num_movies) // 1)
+
+        moviessofar =  0
+        services_string = ','.join(services)
+        watchprovidersstring = "&with_watch_providers=" + services_string + "&watch_region=US"
+        if services == []:
+            watchprovidersstring = ''
+        page = 1
+        while moviessofar < genrescore[genre]:
+            response = requests.get("https://api.themoviedb.org/3/discover/movie?api_key=" + TMDB_api_key +
+                                    "&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + str(page) + "&with_genres=" +
+                                    genre + watchprovidersstring + "&with_watch_monetization_types=flatrate")
+            data = response.json()['results']
+
+            for result in data:
+                if result['title'] not in movies and result['title'] not in watched_movies and moviessofar < genrescore[genre]:
+                    movie = {}
+                    movie['id'] = result['id']
+                    movie['genre_ids'] = result['genre_ids']
+                    sources = sources_from_tmdbID(movie['id'])
+                    if sources != 'None':
+                        sources_with_service = [sources[x] for x in sources if str(sources[x]) in services] 
+                        movie['source'] = sources_with_service
+                        movies[result['title']] = movie
+                        moviessofar += 1
+            page += 1
+            
+            
     return movies
 
 
@@ -106,10 +125,17 @@ def sources_from_tmdbID(tmdbID):
     response = requests.get("https://api.themoviedb.org/3/movie/" + str(tmdbID) + '/watch/providers?api_key=' + TMDB_api_key)
 
     if response.status_code == 200:
-        data = response.json()['results']['US']['flatrate']
-        services = []
+        data = response.json()
+        if 'US' not in data['results']:
+            return "None"
+        if 'flatrate' not in data['results']['US']:
+            return "None"
+        if data['results']['US']['flatrate'] == {}:
+            return "None"
+        data = data['results']['US']['flatrate']
+        services = {}
         for source in data:
-            services.append(source['provider_name'])
+            services[source['provider_name']] = source['provider_id']
         return services
     else:
         raise Exception('TMDB API gave status code  {}'.format(response.status_code))
